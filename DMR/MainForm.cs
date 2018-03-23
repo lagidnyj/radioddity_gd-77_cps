lagidnyj/radioddity_gd-77_cps

commit 322856a31d95ec9d6c16f5ca1fb2851a0fb0d853

Started to add DMRID form and new item on the tree view, but its currently hidden in the tree

DMR/MainForm.cs
@@ -3264,7 +3264,7 @@ namespace DMR
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, null, typeof(RxGroupListForm), 128, -1, 17, RxGroupListForm.data));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ZoneBasicForm), typeof(ZoneForm), 250, -1, 16, ZoneForm.data));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ChannelsForm), typeof(ChannelForm), ChannelForm.CurCntCh, -1, 17, ChannelForm.data));
-			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ScanBasicForm), typeof(NormalScanForm), 64, -1, 16, NormalScanForm.data));
+			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ScanBasicForm), typeof(NormalScanForm), 0, -1, 30, null));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(null, null, null, 0, -1, 17, null));
 			int num = 0;
 			for (int i = 0; i < 2; i++)
@@ -3283,6 +3283,8 @@ namespace DMR
 				}
 				this.lstTreeNodeItem.Add(new TreeNodeItem(null, typeof(VfoForm), null, 2, i, num, VfoForm.data));
 			}
+			// Add new DMR-ID section
+//			this.lstTreeNodeItem.Add(new TreeNodeItem(null, typeof(DMRIDForm), null, 0, -1, 19, null));// 19 is the icon number
 		}
 
 		private DataTable method_19()
@@ -3313,6 +3315,7 @@ namespace DMR
 			dataTable.Rows.Add("0016", "VFO", "00");
 			dataTable.Rows.Add("001600", "VFOA", "0016");
 			dataTable.Rows.Add("001601", "VFOB", "0016");
+//			dataTable.Rows.Add("0017", "DMR-ID", "00");// DMRID
 			return dataTable;
 		}
 

DMR/DMRIDForm.Designer.cs
@@ -1,0 +1,77 @@
+﻿namespace DMR
+{
+	partial class DMRIDForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dmrid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.callsign = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dmrid,
+            this.callsign});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 44);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(273, 436);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// dmrid
+			// 
+			this.dmrid.HeaderText = "DMR ID";
+			this.dmrid.Name = "dmrid";
+			// 
+			// callsign
+			// 
+			this.callsign.HeaderText = "Callsign";
+			this.callsign.Name = "callsign";
+			// 
+			// DMRIDForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(481, 492);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "DMRIDForm";
+			this.Text = "DMR ID";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dmrid;
+		private System.Windows.Forms.DataGridViewTextBoxColumn callsign;
+	}
+}

DMR/DMRIDForm.cs
@@ -1,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DMR
+{
+
+	
+
+
+
+	public partial class DMRIDForm : Form
+	{
+
+		private BindingSource _dataSource = new BindingSource();
+
+		public struct ChangedByte
+		{
+			public byte[] DMRID;
+			public byte[] Callsign;
+
+		}
+
+		public DMRIDForm()
+		{
+			InitializeComponent();
+			loadData();
+		}
+
+		private void loadData()
+		{
+			_dataSource.Add(new DMRIDObject("5053124","VK3KYY"));
+			_dataSource.Add(new DMRIDObject("5053125", "VK3KYY"));
+			_dataSource.Add(new DMRIDObject("5053126", "VK3KYY"));
+			dataGridView1.AutoGenerateColumns = false;
+			dataGridView1.AutoSize = true;
+			dataGridView1.DataSource = _dataSource;
+		}
+
+		public static byte[] StructureArrayToByteArray(ChangedByte[] objs)
+		{
+			int structSize = Marshal.SizeOf(typeof(ChangedByte));
+			int len = objs.Length * structSize;
+			byte[] arr = new byte[len];
+			IntPtr ptr = Marshal.AllocHGlobal(len);
+			for (int i = 0; i < objs.Length; i++)
+			{
+				Marshal.StructureToPtr(objs[i], ptr + i * structSize, true);
+			}
+			Marshal.Copy(ptr, arr, 0, len);
+			Marshal.FreeHGlobal(ptr);
+			return arr;
+		}
+	}
+
+
+	#region "DMR ID Object"
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct DMRIDObject
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+		private byte[] _dmrid;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+		private byte[] _callsign;
+
+		public string dmrid
+		{
+			get
+			{
+				return Encoding.ASCII.GetString(_dmrid);
+			}
+			set
+			{
+				Array.Clear(_dmrid, 0, 4);
+				_dmrid = Encoding.ASCII.GetBytes(value);
+			}
+		}
+
+		public string callsign
+		{
+			get
+			{
+				return Encoding.ASCII.GetString(_callsign);
+			}
+			set
+			{
+				Array.Clear(_callsign, 0, 8);
+				_callsign = Encoding.ASCII.GetBytes(value);
+			}
+		}
+
+		public DMRIDObject(string id, string callsign)
+		{
+
+			_dmrid = Encoding.ASCII.GetBytes(id);
+			_callsign = Encoding.ASCII.GetBytes(callsign);
+		}
+	}	
+	#endregion
+}

DMR/DMRIDForm.resx
@@ -1,0 +1,126 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<root>
+  <!-- 
+    Microsoft ResX Schema 
+    
+    Version 2.0
+    
+    The primary goals of this format is to allow a simple XML format 
+    that is mostly human readable. The generation and parsing of the 
+    various data types are done through the TypeConverter classes 
+    associated with the data types.
+    
+    Example:
+    
+    ... ado.net/XML headers & schema ...
+    <resheader name="resmimetype">text/microsoft-resx</resheader>
+    <resheader name="version">2.0</resheader>
+    <resheader name="reader">System.Resources.ResXResourceReader, System.Windows.Forms, ...</resheader>
+    <resheader name="writer">System.Resources.ResXResourceWriter, System.Windows.Forms, ...</resheader>
+    <data name="Name1"><value>this is my long string</value><comment>this is a comment</comment></data>
+    <data name="Color1" type="System.Drawing.Color, System.Drawing">Blue</data>
+    <data name="Bitmap1" mimetype="application/x-microsoft.net.object.binary.base64">
+        <value>[base64 mime encoded serialized .NET Framework object]</value>
+    </data>
+    <data name="Icon1" type="System.Drawing.Icon, System.Drawing" mimetype="application/x-microsoft.net.object.bytearray.base64">
+        <value>[base64 mime encoded string representing a byte array form of the .NET Framework object]</value>
+        <comment>This is a comment</comment>
+    </data>
+                
+    There are any number of "resheader" rows that contain simple 
+    name/value pairs.
+    
+    Each data row contains a name, and value. The row also contains a 
+    type or mimetype. Type corresponds to a .NET class that support 
+    text/value conversion through the TypeConverter architecture. 
+    Classes that don't support this are serialized and stored with the 
+    mimetype set.
+    
+    The mimetype is used for serialized objects, and tells the 
+    ResXResourceReader how to depersist the object. This is currently not 
+    extensible. For a given mimetype the value must be set accordingly:
+    
+    Note - application/x-microsoft.net.object.binary.base64 is the format 
+    that the ResXResourceWriter will generate, however the reader can 
+    read any of the formats listed below.
+    
+    mimetype: application/x-microsoft.net.object.binary.base64
+    value   : The object must be serialized with 
+            : System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
+            : and then encoded with base64 encoding.
+    
+    mimetype: application/x-microsoft.net.object.soap.base64
+    value   : The object must be serialized with 
+            : System.Runtime.Serialization.Formatters.Soap.SoapFormatter
+            : and then encoded with base64 encoding.
+
+    mimetype: application/x-microsoft.net.object.bytearray.base64
+    value   : The object must be serialized into a byte array 
+            : using a System.ComponentModel.TypeConverter
+            : and then encoded with base64 encoding.
+    -->
+  <xsd:schema id="root" xmlns="" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
+    <xsd:import namespace="http://www.w3.org/XML/1998/namespace" />
+    <xsd:element name="root" msdata:IsDataSet="true">
+      <xsd:complexType>
+        <xsd:choice maxOccurs="unbounded">
+          <xsd:element name="metadata">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" />
+              </xsd:sequence>
+              <xsd:attribute name="name" use="required" type="xsd:string" />
+              <xsd:attribute name="type" type="xsd:string" />
+              <xsd:attribute name="mimetype" type="xsd:string" />
+              <xsd:attribute ref="xml:space" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="assembly">
+            <xsd:complexType>
+              <xsd:attribute name="alias" type="xsd:string" />
+              <xsd:attribute name="name" type="xsd:string" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="data">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
+                <xsd:element name="comment" type="xsd:string" minOccurs="0" msdata:Ordinal="2" />
+              </xsd:sequence>
+              <xsd:attribute name="name" type="xsd:string" use="required" msdata:Ordinal="1" />
+              <xsd:attribute name="type" type="xsd:string" msdata:Ordinal="3" />
+              <xsd:attribute name="mimetype" type="xsd:string" msdata:Ordinal="4" />
+              <xsd:attribute ref="xml:space" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="resheader">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
+              </xsd:sequence>
+              <xsd:attribute name="name" type="xsd:string" use="required" />
+            </xsd:complexType>
+          </xsd:element>
+        </xsd:choice>
+      </xsd:complexType>
+    </xsd:element>
+  </xsd:schema>
+  <resheader name="resmimetype">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name="version">
+    <value>2.0</value>
+  </resheader>
+  <resheader name="reader">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name="writer">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <metadata name="dmrid.UserAddedColumn" type="System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
+    <value>True</value>
+  </metadata>
+  <metadata name="callsign.UserAddedColumn" type="System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
+    <value>True</value>
+  </metadata>
+</root>

GD77_CPS205.csproj
@@ -73,6 +73,12 @@
     <Compile Include="DMR\DownloadContactsForm.Designer.cs">
       <DependentUpon>DownloadContactsForm.cs</DependentUpon>
     </Compile>
+    <Compile Include="DMR\DMRIDForm.cs">
+      <SubType>Form</SubType>
+    </Compile>
+    <Compile Include="DMR\DMRIDForm.Designer.cs">
+      <DependentUpon>DMRIDForm.cs</DependentUpon>
+    </Compile>
     <Compile Include="MainWindow.cs">
       <SubType>Code</SubType>
     </Compile>
@@ -289,6 +295,9 @@
     <EmbeddedResource Include="DMR\ContactsForm.resx" />
     <EmbeddedResource Include="DMR\DeviceInfoForm.resx" />
     <EmbeddedResource Include="DMR\DigitalKeyContactForm.resx" />
+    <EmbeddedResource Include="DMR\DMRIDForm.resx">
+      <DependentUpon>DMRIDForm.cs</DependentUpon>
+    </EmbeddedResource>
     <EmbeddedResource Include="DMR\DtmfContactForm.resx" />
     <EmbeddedResource Include="DMR\DtmfForm.resx" />
     <EmbeddedResource Include="DMR\EmergencyForm.resx" />

commit a5ca27592f37e636fdb4df9e7883bd5f5b2f836d

Fixed issue with Scan list (caused in commit 322856a31d95ec9d6c16f5ca1fb2851a0fb0d853, by accidently changing the scan list menu item)

DMR/MainForm.cs
@@ -3348,7 +3348,7 @@ namespace DMR
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, null, typeof(RxGroupListForm), 128, -1, 17, RxGroupListForm.data));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ZoneBasicForm), typeof(ZoneForm), 250, -1, 16, ZoneForm.data));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ChannelsForm), typeof(ChannelForm), ChannelForm.CurCntCh, -1, 17, ChannelForm.data));
-			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ScanBasicForm), typeof(NormalScanForm), 0, -1, 30, null));
+			this.lstTreeNodeItem.Add(new TreeNodeItem(this.cmsGroup, typeof(ScanBasicForm), typeof(NormalScanForm), 64, -1, 16, NormalScanForm.data));
 			this.lstTreeNodeItem.Add(new TreeNodeItem(null, null, null, 0, -1, 17, null));
 			int num = 0;
 			for (int i = 0; i < 2; i++)